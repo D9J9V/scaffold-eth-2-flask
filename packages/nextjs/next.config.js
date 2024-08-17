4dc13a1af2534394a5b0b237fecc976002ad8d8a
D9J9V <D9J9V@protonmail.com>
2024-08-17 09:37:53 -0600

    Initial commit, install flask py server

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -9,6 +9,14 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
   },
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination: process.env.NODE_ENV === "development" ? "http://127.0.0.1:5328/api/:path*" : "/api/",
+      },
+    ];
+  },
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push("pino-pretty", "lokijs", "encoding");
