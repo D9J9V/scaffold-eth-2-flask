3d9cb5c00752df4dd57a12ec90ae9dbbf286952c
D9J9V <D9J9V@protonmail.com>
2024-08-17 11:10:35 -0600

    refactor next config

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -1,5 +1,3 @@
-// @ts-check
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -9,6 +7,11 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
   },
+  webpack: config => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
   rewrites: async () => {
     return [
       {
@@ -16,11 +19,6 @@ const nextConfig = {
         destination: process.env.NODE_ENV === "development" ? "http://127.0.0.1:5328/api/:path*" : "/api/",
       },
     ];
-  },
-  webpack: config => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
-    config.externals.push("pino-pretty", "lokijs", "encoding");
-    return config;
   },
 };
 
